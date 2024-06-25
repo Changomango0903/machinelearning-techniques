30c491b7e721afe8d106d866447ba8e59fc084d1
Sean Chang <schang09032003@gmail.com>
2024-06-25 01:30:30 +0100

    feat: Added Slope Gradient Descent technique for Linear Regression

--- linear regression/linear_regression.py
+++ linear regression/linear_regression.py
@@ -9,3 +9,13 @@ def get_gradient_at_b(x, y, m, b):
   b_gradient = (-2/len(x)) * diff
   return b_gradient
 
+#Gradient Descent technique for Slope
+#sum of all x_value * (y_value - (slope * x_value + b))
+#multipy sum by -2/number of points
+#m is the current slope guess
+def get_gradient_at_m(x, y, m, b):
+  diff = 0
+  for i in range(len(x)):
+    diff += x[i] * (y[i] - (m * x[i] + b))
+  m_gradient = -2/len(x) * diff
+  return m_gradient
