d47877f2086cb66a7ab4d357465d52d5a1eedf80
Sean Chang <schang09032003@gmail.com>
2024-06-30 03:23:30 +0100

    feat: Implemented Manhattan Distance

--- K-nearest neighbors/distance.py
+++ K-nearest neighbors/distance.py
@@ -5,3 +5,10 @@ def euclidian_distance(p1, p2):
         distance += (p1[i] - p2[i]) ** 2
     return distance ** (1/2)
 
+#calculate "legs" of path to p1, p2 (adjacent and opposite sides of triangle)
+def manhattan_distance(p1, p2):
+    distance = 0
+    for i in range(len(p1)):
+        distance += abs(p1[i] - p2[i])
+    return distance
+
